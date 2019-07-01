dd9a66dae456199767784fb464acbd70c1d73f63
Yongyuan Bai <y47bai@uwaterloo.ca>
2019-07-01 10:54:24 -0400

    removed lower-case named files

--- src/castle_model/Worker.java
+++
@@ -1,9 +0,0 @@
-package castle_model;
-
-public class Worker extends Unit{
-
-	Worker(int x, int y, Player p) {
-		super(x, y, p);
-	}
-	
-}
--- src/castle_model/troop.java
+++
@@ -1,9 +0,0 @@
-package castle_model;
-
-public class Troop extends Unit{
-
-	Troop(int x, int y, Player p) {
-		super(x, y, p);
-	}
-	
-}
